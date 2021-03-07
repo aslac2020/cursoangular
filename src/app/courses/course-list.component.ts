77da7602f00721588b9afe2c69529c7a7095f827
aslac2020 <andredesenvolvedorti@gmail.com>
2021-03-07 00:48:29 -0300

    alterações

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
 
 
@@ -8,6 +8,31 @@ import { Course } from './course';
 
 })
 
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
     courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular : Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 5.4,
+                releaseDate: 'November, 2, 2019'
+            },
+            {
+                id: 1,
+                name: 'Angular : Http',
+                imageUrl: '',
+                price: 45.99,
+                code: 'LKL-1094',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'November, 4, 2019'
+            },
+        ]
+    }
 }
