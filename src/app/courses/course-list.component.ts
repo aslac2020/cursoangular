b11b18d6a3e51c709cb9da4c3b4f6dfc9b0b3e78
aslac2020 <andredesenvolvedorti@gmail.com>
2021-03-07 11:51:56 -0300

    ratings

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -20,7 +20,7 @@ export class CourseListComponent implements OnInit {
                 price: 99.99,
                 code: 'XPS-8796',
                 duration: 120,
-                rating: 5.4,
+                rating: 4.5,
                 releaseDate: 'November, 2, 2019'
             },
             {
--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -2,7 +2,8 @@ import {Component, Input, OnChanges} from '@angular/core'
 
 @Component({
     selector: 'app-star',
-    templateUrl: './star.component.html'
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
 
 })
 
@@ -13,7 +14,7 @@ export class StarComponent implements OnChanges {
   starWidth: number;
 
     ngOnChanges(): void {
-        this.starWidth = this.rating * 94 / 5;
+        this.starWidth = this.rating * 74 / 5;
 
     }
 
